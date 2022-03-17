08f048ab626f8342e37841d2fadbd114f3e82e68
Ryan Butler <ryancbutler@users.noreply.github.com>
2022-03-17 09:10:03 -0500

    update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,12 @@ ls -alF /mnt/test
 
 ls -alF /myawesomefileshare
 
+touch /myawesomefileshare/${MYVAR}.txt
+
+apt-get update
+apt-get install bc -y
+
+echo "scale=5000; 4*a(1)" | bc -l
 echo "DONE WITH SCRIPT"
 
 if [ $? -ne 0 ]; then
